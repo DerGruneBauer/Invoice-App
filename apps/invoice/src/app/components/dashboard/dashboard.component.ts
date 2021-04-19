579546f9ddf8dafec80a18740fc06c4eea8a613a
remington <remigreenbauer@gmail.com>
2021-04-18 20:59:59 -0400

    media query UI dashboard completed

--- apps/invoice/src/app/components/dashboard/dashboard.component.ts
+++ apps/invoice/src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { InvoiceCardComponent } from '../invoice-card/invoice-card.component';
+import { InvoiceService } from 'apps/invoice/src/app/invoice.service';
 
 @Component({
   selector: 'der-grune-bauer-dashboard',
@@ -8,14 +9,15 @@ import { InvoiceCardComponent } from '../invoice-card/invoice-card.component';
 })
 export class DashboardComponent implements OnInit {
 
-  invoices: any[] = [
-    {code: "#RT3080", date: "August 19, 2020", amount: "$4,000.90", status: "pending", client: "Boris Crowther"},
-    {code: "#RT3080", date: "August 19, 2020", amount: "$4,000.90", status: "draft", client: "Boris Crowther"},
-    {code: "#RT3080", date: "August 19, 2020", amount: "$4,000.90", status: "paid", client: "Boris Crowther"},
-  ];
-  constructor() { }
+  screenWidth: any = window.screen.width;
+  
+  constructor(private invoiceService: InvoiceService) { }
 
   ngOnInit(): void {
+  }
+
+  get invoiceNumber() {
+    return this.invoiceService.getInvoiceNumber();
   }
 
 }
--- apps/invoice/src/app/components/invoice-card/invoice-card.component.ts
+++ apps/invoice/src/app/components/invoice-card/invoice-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InvoiceService } from 'apps/invoice/src/app/invoice.service';
 
 @Component({
   selector: 'der-grune-bauer-invoice-card',
@@ -7,20 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvoiceCardComponent implements OnInit {
 
+  screenWidth: any = window.screen.width;
+
   invoices: any[] = [
     {code: "RT3080", date: "August 19, 2020", amount: "$4,000.90", status: "Pending", client: "Boris Crowther"},
     {code: "RT3080", date: "August 19, 2020", amount: "$4,000.90", status: "Paid", client: "Boris Crowther"},
     {code: "RT3080", date: "August 19, 2020", amount: "$4,000.90", status: "Draft", client: "Boris Crowther"},
-    {code: "RT3080", date: "August 19, 2020", amount: "$4,000.90", status: "Draft", client: "Boris Crowther"},
+    {code: "RT3080", date: "August 19, 2020", amount: "$4,000.90", status: "Pending", client: "Boris Crowther"},
     {code: "RT3080", date: "August 19, 2020", amount: "$4,000.90", status: "Draft", client: "Boris Crowther"},
     {code: "RT3080", date: "August 19, 2020", amount: "$4,000.90", status: "Draft", client: "Boris Crowther"},
     {code: "RT3080", date: "August 19, 2020", amount: "$4,000.90", status: "Draft", client: "Boris Crowther"},
   ];
 
-  constructor() { }
+  constructor(private invoiceService: InvoiceService) { }
   
 
+
   ngOnInit(): void {
+    
   }
+
 
 }
